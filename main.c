42ba37ea289b872c07942fcc86d9e3edbb14286d
n1k3code <65698426+n1k3code@users.noreply.github.com>
2024-01-02 23:48:00 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <Windows.h>
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+        MessageBox(0, L"DLL Injetada! Cyber Security 2024 by: N1k3Code", L"Desafio 02", MB_OK);
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
